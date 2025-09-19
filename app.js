0393b44a629a0a9b1eb066adba6deb789d41408a
abdullahjarif <jarif.abdullah2001@gmail.com>
2025-09-19 20:21:18 +0600

    push pop

---
+++ app.js
@@ -0,0 +1,26 @@
+// variable
+// const and let
+
+// let name = "Jarif";
+// name = "Abdullah Jarif";
+
+// const countryName = "Bangladesh";
+// const num = 18;
+// // if (num >= 20 && num <= 30) {
+// if (num >= 20 || num <= 30) {
+//   console.log("Yes");
+// } else {
+//   console.log("No");
+// }
+
+const friends = ["Joe", "Rahim", "Karif", "Jarif", "Arif"];
+console.log(friends);
+console.log(friends.length);
+console.log(friends[1]);
+friends.pop();
+friends.pop();
+friends.push("Amin");
+friends.unshift("Abdullah")
+console.log(friends);
+
+
