ae0b4e2ebe50213e924722377c53fff8f4c7d8fa
abdullahjarif <jarif.abdullah2001@gmail.com>
2025-09-20 19:06:59 +0600

    complete module-1

--- app.js
+++ app.js
@@ -15,12 +15,47 @@
 
 const friends = ["Joe", "Rahim", "Karif", "Jarif", "Arif"];
 console.log(friends);
-console.log(friends.length);
-console.log(friends[1]);
-friends.pop();
-friends.pop();
-friends.push("Amin");
-friends.unshift("Abdullah")
-console.log(friends);
+// console.log(friends.length);
+// console.log(friends[1]);
+// friends.pop();
+// friends.pop();
+// friends.push("Amin");
+// friends.unshift("Abdullah");
+// friends.shift("Abdullah");
+// console.log(friends);
+// console.log(friends.slice(2, 3));
+// console.log(friends.splice(2, 3));
+
+// for (let ind = 0; ind < friends.length; ind++) {
+//   // console.log(friends);
+//   const element = friends[ind];
+//   console.log(element);
+// }
+
+// function sum(a, b) {
+//   console.log(arguments);
+
+//   const result = a + b;
+//   return result;
+// }
+
+// const output = sum(4, 5);
+// console.log(output);
+
+const person = {
+  lname: "Jarif",
+  fname: "Abdullah",
+  age: 23,
+  address: {
+    street: "3",
+    block: "A",
+  },
+  friends: friends
+};
+person.fname = "Hero";
+console.log(person);
+console.log(person.friends);
+console.log(person.friends[1]);
+console.log(person.address.block);
 
 
