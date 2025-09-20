6c7cc2311140693db2223b86ca1a74ca7c858861
abdullahjarif <jarif.abdullah2001@gmail.com>
2025-09-20 20:04:09 +0600

    complete module-2

--- template.js
+++ template.js
@@ -5,3 +5,33 @@ const lname = "Jarif";
 
 console.log(fname + " " + lname);
 console.log(`My name is ${fname} ${lname}`);
+
+// arrow function
+const sum = (a, b) => a + b;
+console.log(sum(4, 5));
+
+// multiple line hoile second bracket use korte hobe
+
+// rest operator
+const total = (...rest) => {
+  console.log(rest);
+};
+total(1, 2, 3); // give an array
+
+const multiply = (a, b, c) => {
+  const result = a * b * c;
+  return result;
+};
+console.log(multiply(4, 5, 8));
+
+// ... spread operator
+
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers);
+console.log(...numbers);
+
+const newArr = [...numbers, 10, 100];
+console.log(newArr);
+
+
+
