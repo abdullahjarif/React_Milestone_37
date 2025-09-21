f60496d704f0f89b4b8b8725f7a132523aa1c91b
abdullahjarif <jarif.abdullah2001@gmail.com>
2025-09-21 20:15:21 +0600

    running module 6

---
+++ shortcut.js
@@ -0,0 +1,11 @@
+// truthy value
+// false value --> false, 0, -0, null, undefined
+
+// check truth or false value
+const test = 1;
+const result = test ? "true value" : "false value";
+console.log(result);
+
+const num = 18;
+const result2 = num >= 5 && num < 15 ? "true value" : "false value";
+console.log(result2);
