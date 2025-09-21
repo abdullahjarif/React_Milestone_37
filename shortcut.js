abe9efc9bd961389d5a32c153be7672d335adc89
abdullahjarif <jarif.abdullah2001@gmail.com>
2025-09-21 21:48:30 +0600

    end module 6

--- shortcut.js
+++ shortcut.js
@@ -2,10 +2,34 @@
 // false value --> false, 0, -0, null, undefined
 
 // check truth or false value
-const test = 1;
-const result = test ? "true value" : "false value";
-console.log(result);
+// const test = 1;
+// const result = test ? "true value" : "false value";
+// console.log(result);
 
-const num = 18;
-const result2 = num >= 5 && num < 15 ? "true value" : "false value";
-console.log(result2);
+// const num = 18;
+// const result2 = num >= 5 && num < 15 ? "true value" : "false value";
+// console.log(result2);
+
+const isActive = true;
+// console.log(!isActive);
+const showUser = () => console.log("Show user green");
+const hideUser = () => console.log("Hide user red");
+
+// isActive ? showUser() : hideUser();
+isActive && showUser();
+
+
+const num = '10';
+const addResult = +num;
+console.log(typeof addResult);
+
+const add = num + 10;
+const sub = num - 10;
+
+console.log(typeof num);
+console.log(typeof add);
+console.log(add);
+console.log(typeof sub);
+console.log(sub);
+
+
