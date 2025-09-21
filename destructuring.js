efc3d3405aeaa78186627e8ced7155d1769b61b5
abdullahjarif <jarif.abdullah2001@gmail.com>
2025-09-21 11:06:36 +0600

    running module-4

---
+++ destructuring.js
@@ -0,0 +1,7 @@
+const friends = ["Jarif", "Kashem", "Hasem", "Jalil", "Fahad", "Ahad", "Arif"];
+// const element1 = friends[0];
+// const element2 = friends[1];
+// const element3 = friends[2];
+// const element4 = friends[3];
+const [element1, element4, element3, element2] = friends;
+console.log(element1, element2, element3, element4);
