0bc5c49708395e62c802f8a142ac69bd224e3ee1
abdullahjarif <jarif.abdullah2001@gmail.com>
2025-09-22 12:51:00 +0600

    running last module

--- notation.js
+++ notation.js
@@ -2,9 +2,14 @@ const person = {
     name: "Jarif",
     age: 25,
     friends: ["Alom", "Babul", "Abul", "Kalam"],
-    salary: 25000
+    salary: 25000,
+    10: "Secret code"
 }
 
 // const name = person.name; // dot notation
 const name = person["name"] // bracket notation
 console.log(name);
+
+const secretCode = person["10"];
+console.log(secretCode);
+
